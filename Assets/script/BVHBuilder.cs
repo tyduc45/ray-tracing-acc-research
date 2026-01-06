64ad7fa99ee32d6e06e85c45006e1f1315b4e7e0
tyduc45 <3381274436@qq.com>
2026-01-06 16:47:04 +0800

    stress test dragon 80K

--- Assets/script/BVHBuilder.cs
+++ Assets/script/BVHBuilder.cs
@@ -27,7 +27,7 @@ public class BVHBuilder
         int nodeIdx = _flatNodes.Count;
         _flatNodes.Add(default);
 
-        if (count <= 50) // leaf node 
+        if (count <= 100) // leaf node 
         {
             _flatNodes[nodeIdx] = new BVHNode
             {
